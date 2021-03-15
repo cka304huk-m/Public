14ad41177986bc99ec859c522241a5e6a2fa97f7
CK@304HuK <50353911+cka304huk-m@users.noreply.github.com>
2021-03-15 23:08:37 +0300

    Осваиваю ООП

---
+++ azbuka_morze/translate.py
@@ -0,0 +1,53 @@
+import azbukamorze
+
+def main():
+    show_choice()
+
+def show_menu():
+    """Показывает меню"""
+    print(
+        """
+        Меню:
+        0 - Выход.
+        1 - Перевести слово или предложение на азбуку
+        2 - Перевести азбуку в символ или слово/предложение.
+        """
+    )
+
+def show_choice():
+    """Меню"""
+    choice = None
+    while choice != "0":
+        show_menu()
+        choice = input("\nВаш выбор - ")
+        if choice == "0":
+            print("До свидания!")
+        elif choice == "1":
+            translate_words_in_morze()
+        elif choice == "2":
+            translate_morze_in_words()
+
+
+def translate_words_in_morze():
+    """Переводит слова в азубку морзе."""
+    code = ''
+    words = input("Введите слово или предложение: ")
+    AM = azbukamorze.AzbukaMorze(words)
+    for l in AM.get_string().lower():
+        if l in AM.dict_az_morze():
+            code += AM.dict_az_morze()[l].replace(' ', '') + " "
+    print(code)
+
+
+def translate_morze_in_words():
+    """Перевожу код азбуки в обратно в текст."""
+    word = ""
+    code = input("Код для расшифровки: ").split()
+    AM = azbukamorze.AzbukaMorze(code)
+    for c in code:
+        for k, v in AM.dict_az_morze().items():
+            if v == c:
+                word += k + ' '
+    print(f"Расшифровываю, это - {word}")
+
+main()
