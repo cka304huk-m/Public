952006aa787418e8d6190fa28c47f2e0a751fcfe
CK@304HuK <50353911+cka304huk-m@users.noreply.github.com>
2021-04-14 19:13:40 +0300

    Добавил вызов загрузки файла

--- backup_dir/main.py
+++ backup_dir/main.py
@@ -1,6 +1,7 @@
 # Бэкапер директории с возможностью
 # исключать некоторые файлы.
 from backup import Backup
+import upload
 
 def main():
 
@@ -16,6 +17,16 @@ def main():
     # что исключить из резервной копии.
     exclude_files = my_backup.set_exclude_files(my)
 
-    my_backup.zip_file(exclude_files)
+    # Создаю ахив и выкидываю из него файлы которые
+    # присутсвуют в исключениях.
+    namef = my_backup.zip_file(exclude_files)
+
+    print()
+    up_f = input('Загрузить файл? ДА/НЕТ ')
+    if up_f.lower() == 'да':
+        # Загрузка файла на Mega.NZ
+        upload.upload_file(namef)
+    else:
+        print('Вы пропустили загрузку файла.')
 
 main()
