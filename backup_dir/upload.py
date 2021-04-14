c07d672dc0053ffc619489059e490b8d66bc9c30
CK@304HuK <50353911+cka304huk-m@users.noreply.github.com>
2021-04-14 19:14:32 +0300

    Добавил загрузку в папку если её указать.

--- backup_dir/upload.py
+++ backup_dir/upload.py
@@ -1,24 +1,31 @@
 # Загрузка файлов на облачное хранилище.
 from mega import Mega
 
-def upload_file():
+def upload_file(namef):
 
     # Создаю экзепляр.
     mega = Mega()
 
-    email = 'ваша почта'
-    password = 'ваш пароль'
-    filename = input('название файла для загрузки: ')
+    email = 'электронная почта'
+    password = 'пароль'
+
+    # Название файла.
+    filename = namef
+    folder = input('папка назначения: ')
 
     # Захожу на облачное хранилище под
     # своим аккаунтом.
     m = mega.login(email, password)
 
     # Загружаю файл на облачное хранилище.
-    file = m.upload(filename)
-    # Получаю ссылку на него.
-    link = m.get_upload_link(file)
-    print(f'Файл успешно загружен на Mega.NZ')
+    if folder:
+        up_folder = m.find(folder)
+        file = m.upload(filename, up_folder[0])
+        # Получаю ссылку на него.
+        link = m.get_upload_link(file)
+    else:
+        file = m.upload(filename)
+        # Получаю ссылку на него.
+        link = m.get_upload_link(file)
+    print(f'Файл {namef} успешно загружен на Mega.NZ')
     print(f'Ccылка на него: {link}')
-
-upload_file()
