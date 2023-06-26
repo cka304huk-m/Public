63f329958454dafbccabb55bd4b280ebbacb214b
CK@304HuK <50353911+cka304huk-m@users.noreply.github.com>
2023-06-26 20:21:41 +0300

    Главный файл

---
+++ azbuka_morze/main.py
@@ -0,0 +1,28 @@
+from azbuka import *
+
+def main():
+    choice = int(input("1 - в морзе, 2 - в текст: "))
+    string = input('Введите символ, слово или предложение: ')
+
+    result = convert(choice, string)
+
+    print(result)
+
+def convert(choice, string):
+    """Конвертация на основе выбора пользователя."""
+    word = Morse()
+    if choice == 1:
+        string = string.upper()
+        rezult = ''
+        for s in string:
+            rezult += word.convertInMorse(s) + ' '
+    elif choice == 2:
+        string = string.upper().split()
+        rezult = ''
+        for s in string:
+            rezult += word.convertInText(s) + ' '
+
+    return rezult
+
+if __name__ == '__main__':
+    main()
